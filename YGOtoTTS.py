5170059780ed8317ff5bbd8b2d3a5b0fe3c0b200
Jansen Tan <jansenmtan@gmail.com>
2019-07-19 12:34:03 -0500

    Implement making a TTS saved object .json file
    also forgot to commit sending image as base64

--- YGOtoTTS.py
+++ YGOtoTTS.py
@@ -1,10 +1,9 @@
-# TODO: Implement making a TTS saved object .json file
-
 import os
 import requests
 import urllib.request
 from PIL import Image
 import json
+from base64 import b64encode
 
 
 def ceil_div(a, b):
@@ -45,7 +44,7 @@ def get_decklist_images(decklist_dict, extension=".jpg"):
 def make_deck_image():
     # Current dir is the deck images folder
 
-    list_dir = os.listdir()
+    list_dir = os.listdir(".")
     deck_size = len(list_dir)
 
     (card_width, card_height) = Image.open(list_dir[0]).size
@@ -70,7 +69,7 @@ def make_deck_image():
 def make_decklist_dict(ydk_filename, decklist_name):
     print("Getting info for {}...".format(decklist_name))
 
-    deck_list = []
+    decklist = []
 
     with open(ydk_filename, "r") as ydk_file:
         deck_name = ""
@@ -91,7 +90,7 @@ def make_decklist_dict(ydk_filename, decklist_name):
                         "cards": card_list
                     }
 
-                    deck_list.append(deck_dict)
+                    decklist.append(deck_dict)
 
                 deck_name = stripped[1:]
                 card_list = []
@@ -102,7 +101,7 @@ def make_decklist_dict(ydk_filename, decklist_name):
 
     decklist_dict = {
         "name": decklist_name,
-        "decks": deck_list
+        "decks": decklist
     }
 
     print("Done!")
@@ -116,32 +115,102 @@ def get_imgur_link(img_path):
     :param img_path: Path to image on disk
     :return: img_url - Link to image
     """
+
     client_id = "8b9d897b9a78e50"
-    url = "http://api.imgur.com/3/upload.json"
+    url = "https://api.imgur.com/3/image"
     headers = {"Authorization": "Client-ID {}".format(client_id)}
 
     response = requests.post(
         url,
-        headers=headers,
         data={
-            "image": img_path,
-            "type": file,
-        }
+            "image": b64encode(open(img_path, "rb").read()),
+            "type": "base64"
+        },
+        headers=headers
     )
 
     img_url = ""
 
     if response:
-        img_url = response.json()["data"]["link"]
+        img_url = "{}\n".format(response.json()["data"]["link"])
     else:
         # I don't know how to properly handle errors
-        print("Error trying to upload {}".format(img_path))
-        print("Code {}".format(response.status_code))
+        print(response.json()["data"]["error"])
 
     del response
 
     # If there's an error, just sit and cry
     return img_url
+
+
+def make_tts_object(decklist_dict, img_urls):
+    object_template = {
+        "Transform": {
+            "posX": 0.0,
+            "posY": 0.0,
+            "posZ": -6.50,
+            "rotX": 0.0,
+            "rotY": 0.0,
+            "rotZ": 0.0,
+            "scaleX": 1.0,
+            "scaleY": 1.0,
+            "scaleZ": 1.0
+        },
+        "Nickname": "",
+        "Description": "",
+    }
+
+    card_template = object_template.update({
+        "Name": "Card",
+        "CardID": 100
+    })
+
+    deck_template = object_template.update({
+        "Name": "DeckCustom",
+        "DeckIDs": [],  # cardIDs go here
+        "CustomDeck": {},  # image urls go here
+        "ContainedObjects": []  # cards go here
+    })
+
+    decklist_template = object_template.update({
+        "Name": "Bag",
+        "ContainedObjects": [],  # decks go here
+
+    })
+
+    decks = []
+    for deck_idx, deck in enumerate(decklist_dict["decks"]):
+        deck_id = deck_idx + 1
+        deck_ids = [100*deck_id + card_idx for card_idx in range(len(deck["cards"]))]
+        cards = [card_template.update({
+            "Nickname": card["name"],
+            "Description": card["desc"],
+            "CardID": deck_ids[card_idx]
+        }) for card_idx, card in enumerate(deck["cards"])]
+
+        decks.append(deck_template.update({
+            "Nickname": "{} {} Deck".format(decklist_dict["name"], deck["name"].capitalize()),
+            "DeckIDs": deck_ids,
+            "CustomDeck": {
+                str(deck_id): {
+                    "FaceURL": img_urls[deck_idx],
+                    # Image of the back of card found in another mod:
+                    "BackURL": "http://cloud-3.steamusercontent.com/ugc/925921299334738938/83EE3D4F457FE0CD9251F7318E9FE6CAC92D6FF9/"
+                }
+            },
+            "ContainedObjects": cards
+        }))
+
+    decklist_tts_obj = decklist_template.update({
+        "Nickname": decklist_dict["name"],
+        "ContainedObjects": decks
+    })
+
+    tts_object = {
+        "ObjectStates": [decklist_tts_obj]
+    }
+
+    return tts_object
 
 
 # TERMINOLOGY:
@@ -153,11 +222,13 @@ def get_imgur_link(img_path):
 #   side.
 
 # Is the absolute path of the program/.py file
-# decklists_path = os.path.dirname(os.path.realpath(__file__))
-decklists_path = "C:/Users/Jansen/Documents/Yu-Gi-Oh/Decks"
+decklists_path = os.path.dirname(os.path.realpath(__file__))
 
 os.chdir(decklists_path)
 
+# TODO: Get only directories/folders, not files
+# could implement an asynchronous process to only have to iterate through
+#   the deck info once, but I am ignorant
 for decklist_name in os.listdir():
     decklist_path = os.path.join(decklists_path, decklist_name)
     os.chdir(decklist_path)
@@ -191,11 +262,22 @@ for decklist_name in os.listdir():
                         os.chdir("..")
                         deck_image.save("{}.png".format(deck["name"]))
 
-                with open("deck_image_urls.txt", "w") as deck_image_urls:
-                    for img in png_imgs:
-                        deck_image_urls.writelines(get_imgur_link(img))
+                os.chdir(decklist_path)
 
+                with open("deck_image_urls.txt", "w") as image_url_file:
+                    for img in png_imgs:
+                        img_path = os.path.join("images", img)
+                        image_url_file.writelines(get_imgur_link(img_path))
 
             os.chdir(decklist_path)
 
-# back_img_url = "http://cloud-3.steamusercontent.com/ugc/925921299334738938/83EE3D4F457FE0CD9251F7318E9FE6CAC92D6FF9/"
+            tts_obj_path = "{}.json".format(decklist_name)
+            if tts_obj_path not in dir_list:
+                with open("deck_image_urls.txt", "r") as image_url_file:
+                    deck_image_urls = image_url_file.read().split("\n")
+
+                with open(tts_obj_path, "w") as tts_json:
+                    json.dump(make_tts_object(decklist_dict, deck_image_urls), tts_json)
+
+            os.chdir(decklist_path)
+
