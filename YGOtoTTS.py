f2c2b90f30faee1cbc83463016cc940d2a9fd593
Jansen Tan <jansenmtan@gmail.com>
2019-08-02 13:51:41 -0500

    Import fix from main
    i dont know how to use git correctly (for now ;))

--- YGOtoTTS.py
+++ YGOtoTTS.py
@@ -13,7 +13,6 @@
 #     You should have received a copy of the GNU General Public License
 #     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-# TODO: Migrate over to anonfiles.com
 import os
 import requests
 import urllib.request
