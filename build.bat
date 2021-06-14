71098c9ba43c9324e7240b7aa1d4cc184e0b18e7
Jansen Tan <jansenmtan@gmail.com>
2021-06-13 19:55:53 -0500

    fix: rename main file `core.py` to `__main__.py`
    this way, the module can be run as a module

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
 python -m pip install pyinstaller
 python -m pip install -r "./requirements.txt"
-python -OO -m PyInstaller -F -n "YGOtoTTS-win32" "./YGOtoTTS/core.py"
+python -OO -m PyInstaller -F -n "YGOtoTTS-win32" "./YGOtoTTS/__main__.py"
--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 python3 -m pip install pyinstaller
 python3 -m pip install -r "requirements.txt"
-python3 -OO -m PyInstaller -F -n "YGOtoTTS-$(uname -s)" "./YGOtoTTS/core.py"
+python3 -OO -m PyInstaller -F -n "YGOtoTTS-$(uname -s)" "./YGOtoTTS/__main__.py"
